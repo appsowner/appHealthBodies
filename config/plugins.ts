f090ed0c7fa7b3f2c4105283d66862d5b5f61969
AO AppsOwner SPA <110705397+appsowner@users.noreply.github.com>
2025-07-09 13:45:24 -0400

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'smtp',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: true, // true para 465, false para 587
+      },
+      settings: {
+        defaultFrom: env('EMAIL_FROM'),
+        defaultReplyTo: env('EMAIL_REPLY_TO'),
+      },
+    },
+  },
+});
